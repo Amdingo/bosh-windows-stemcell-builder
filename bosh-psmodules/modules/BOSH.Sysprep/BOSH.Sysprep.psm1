99fcca2c6db1f96a5fc945abb7b2337b67f293ed
Jason Smith <jsmith@pivotal.io>
2018-03-27 14:01:13 -0400

    Disable domain sysvol and machine policies
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

--- bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.psm1
+++ bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.psm1
@@ -12,11 +12,11 @@ function Enable-LocalSecurityPolicy {
     Write-Log "Starting LocalSecurityPolicy"
 
     # Convert registry.txt files into registry.pol files
-    $MachineDir="$PolicySource/DomainSysvol/GPO/Machine"
-    LGPO.exe /r "$MachineDir/registry.txt" /w "$MachineDir/registry.pol"
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "Generating policy: Machine"
-    }
+    # $MachineDir="$PolicySource/DomainSysvol/GPO/Machine"
+    # LGPO.exe /r "$MachineDir/registry.txt" /w "$MachineDir/registry.pol"
+    # if ($LASTEXITCODE -ne 0) {
+    #     Write-Error "Generating policy: Machine"
+    # }
 
     $UserDir="$PolicySource/DomainSysvol/GPO/User"
     LGPO.exe /r "$UserDir/registry.txt" /w "$UserDir/registry.pol"
@@ -25,10 +25,10 @@ function Enable-LocalSecurityPolicy {
     }
 
     # Apply policies
-    LGPO.exe /g "$PolicySource/DomainSysvol" /v
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "Applying policy: $PolicySource/DomainSysvol"
-    }
+    # LGPO.exe /g "$PolicySource/DomainSysvol" /v
+    # if ($LASTEXITCODE -ne 0) {
+    #     Write-Error "Applying policy: $PolicySource/DomainSysvol"
+    # }
 
     Write-Log "Ending LocalSecurityPolicy"
 }
