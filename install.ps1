8f20029ca464f1c32b3c54f9c57518d75fcc47ff
Michael Tang <mitang@pivotal.io>
2018-04-23 14:41:29 -0400

    Added a powershell script to prepare an image for stembuild
    - external dependency:  lgpo.exe must be downloaded from Microsoft and
    installed in c:\Windows
    - this script was tested by adding it to a zip archive with agent.zip,
    bosh-psmodules.zip, and OpenSSH-Win64.zip, unarchiving it on the image
    to be prepped, and executing the script.  Note that the last line is
    commented-out -- it must be uncommented for Sysprep to be invoked.
    
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

---
+++ install.ps1
@@ -0,0 +1,20 @@
+﻿Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip "${PSScriptRoot}\bosh-psmodules.zip"  "C:\Program Files\WindowsPowerShell\Modules"
+powershell Install-CFFeatures
+powershell Protect-CFCell
+
+powershell Install-Agent -IaaS vsphere -agentZipPath "${PSScriptRoot}\agent.zip"
+powershell Install-SSHD -SSHZipFile "${PSScriptRoot}\OpenSSH-Win64.zip"
+
+Optimize-Disk
+Compress-Disk
+
+#Invoke-Sysprep -IaaS vsphere
