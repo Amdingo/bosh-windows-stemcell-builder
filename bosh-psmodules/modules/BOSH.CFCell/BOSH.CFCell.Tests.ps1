4d5804f72537477dc98fe2355aeb7aeef6645689
Zander Mackie <amackie@pivotal.io>
2018-10-02 12:25:32 -0400

    Major refactor of packer provisioner generation
     - Provisioners now generated by OS Version specific ERB templates
    
     [#159702656](https://www.pivotaltracker.com/story/show/159702656)
    
    Co-authored-by: Jackson Feeny <jfeeny@pivotal.io>

--- bosh-psmodules/modules/BOSH.CFCell/BOSH.CFCell.Tests.ps1
+++ bosh-psmodules/modules/BOSH.CFCell/BOSH.CFCell.Tests.ps1
@@ -59,22 +59,12 @@ Describe "Protect-CFCell" {
 }
 
 Describe "Remove-DockerPackage" {
+    It "should bail out early if docker is already installed but we cannot test this either" {
+        # Pester has issues mocking functions that use validateSet See: https://github.com/pester/Pester/issues/734
+    }
+
     It "Is impossible to test this" {
-        # Pest has issues mocking functions that use validateSet See: https://github.com/pester/Pester/issues/734
-#        Mock Uninstall-Package { } -ModuleName BOSH.CFCell
-#        Mock Write-Log { } -ModuleName BOSH.CFCell
-#        Mock Uninstall-Module { } -ParameterFilter { $Name -eq "DockerMsftProvider" -and $ErrorAction -eq "Ignore" } -ModuleName BOSH.CFCell
-#        Mock Get-HNSNetwork { "test-network" } -ModuleName BOSH.CFCell
-#        Mock Remove-HNSNetwork { } -ModuleName BOSH.CFCell
-#        Mock remove-DockerProgramData { } -ModuleName BOSH.CFCell
-#
-#        { Remove-DockerPackage } | Should -Not -Throw
-#
-#        Assert-MockCalled Uninstall-Package -Times 1
-#        Assert-MockCalled Uninstall-Module -Times 1 -Scope It -ParameterFilter { $Name -eq "DockerMsftProvider" -and $ErrorAction -eq "Ignore" } -ModuleName BOSH.CFCell
-#        Assert-MockCalled Get-HNSNetwork -Times 1 -Scope It -ModuleName BOSH.CFCell
-#        Assert-MockCalled Remove-HNSNetwork -Times 1 -Scope It -ParameterFilter { $ArgumentList -eq "test-network" } -ModuleName BOSH.CFCell
-#        Assert-MockCalled remove-DockerProgramData-Times 1 -Scope It -ModuleName BOSH.CFCell
+        # Pester has issues mocking functions that use validateSet See: https://github.com/pester/Pester/issues/734
     }
 }
 
